08e139937781e4efe7b6748a0dfd3b8c981abe56
Abdelrhman Mohamed <102931745+AbdElrhman1997@users.noreply.github.com>
2022-12-15 21:00:26 +0200

    new change in server file

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 const cors = require("cors");
 const config = require("./config.json");
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // Routes
 const productRouter = require("./routes/produts");
 const userRouter = require("./routes/users");
-const imageRouter = require("./routes/images");
 
-app.use(express.json());
 app.use(cors());
-app.use(morgan("tiny"));
+app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
-app.use("/api/images", imageRouter);
-app.use("/", express.static("public/Images"));
 
 mongoose
-  .connect(config.localDB, () => {
-    console.log("connect to db done");
-  })
+  .connect(config.localDB)
   .then(() => {
-    app.listen(port, () => {
-      console.log(`app runing in port ${port} ....`);
+    app.listen(PORT, () => {
+      console.log(`app runing in port ${PORT} ....`);
     });
   })
   .catch((err) => {
